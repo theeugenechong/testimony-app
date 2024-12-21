ec8a56f7f03b1bddcad6eead6000593faf2a555b
Eugene Chong <eugenec@blackmagicdesign.com>
2024-12-22 00:21:25 +0800

    Uncomment metadata import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { AuthProvider } from './contexts/AuthContext';
 import { Header } from './components/Header';
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
