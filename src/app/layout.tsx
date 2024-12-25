44ea26860e3c569efbfd48e8d9c7ddf29b103094
Eugene Chong <eugenec@blackmagicdesign.com>
2024-12-25 18:26:15 +0800

    Add metadata with title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { AuthProvider } from './contexts/AuthContext';
 import { Header } from './components/Header';
-// import type { Metadata } from "next";
+import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -13,6 +13,11 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: "CCC Year End Combined Service 2024",
+  description: "Testimony submissions for CCC Year End service",
+};
 
 export default function RootLayout({
   children,
